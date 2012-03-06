6d93655850f2c058b2c66ff845416da5ba2e4aef
Scott Campbell <scott@scottcampbell.com.au>
2012-03-06 16:00:39 +0800

    Clear blank values on submit

--- jquery.enableplaceholder.js
+++ jquery.enableplaceholder.js
@@ -1,37 +1,44 @@
 /**
- *
- * @Author Scott Campbell
- * @Website github.com/scottc/
- * @Version 1.1a
- *
- * Adding placeholder support for those browsers who don't support it yet.
- *
- */
- 
-(function( $ ){
-	$.fn.enableplaceholder = function() {
-		
-		if('placeholder' in document.createElement('input')) return this;
-		
-		return this.each(function(){ var $this = $(this);
-			//initalize
-			if($this.val() == "")
-				$this.val($this.attr("placeholder"));
-			 
-			//focus event handlers
-			$this.focus(function(){
-				if($this.val() == $this.attr("placeholder"))
-					$this.val("");
-			}).blur(function(){
-				if($this.val() == "")
-					$this.val($this.attr("placeholder"));
-			});
-			
-			//reset button fix
-			$this.closest("form").bind("reset",function(){
-				$this.val($this.attr("placeholder"));
-				return false;
-			});
-		});
-	};
-})( jQuery );
+*
+* @Author Scott Campbell
+* @Website github.com/scottc/
+* @Version 1.2a
+*
+* Adding placeholder support for those browsers who don't support it yet.
+*
+*/
+
+(function ($) {
+    $.fn.enableplaceholder = function () {
+
+        if ('placeholder' in document.createElement('input')) return this;
+
+        return this.each(function () {
+            var $this = $(this);
+            //initalize
+            if ($this.val() == "")
+                $this.val($this.attr("placeholder"));
+
+            //focus event handlers
+            $this.focus(function () {
+                if ($this.val() == $this.attr("placeholder"))
+                    $this.val("");
+            }).blur(function () {
+                if ($this.val() == "")
+                    $this.val($this.attr("placeholder"));
+            });
+
+            //reset button fix
+            $this.closest("form").bind("reset", function () {
+                $this.val($this.attr("placeholder"));
+                return false;
+            });
+
+            //clear blank values on submit
+            $this.closest("form").bind("submit", function () {
+                if ($this.val() == $this.attr("placeholder"))
+                    $this.val("");
+            });
+        });
+    };
+})(jQuery);
