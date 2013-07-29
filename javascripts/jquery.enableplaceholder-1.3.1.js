d1e750d4bd6fd83e4f27cfc8e457150b06685447
Scott Campbell <scott@scottcampbell.com.au>
2013-07-29 20:29:30 +0800

    Update and rename jquery.enableplaceholder-1.3.0.js to jquery.enableplaceholder-1.3.1.js

---
+++ javascripts/jquery.enableplaceholder-1.3.1.js
@@ -0,0 +1,51 @@
+/*!
+*
+* @Author Scott Campbell
+* @Website github.com/scottc/
+* @Version 1.3.1
+* @License GPLv3
+*
+*/
+
+(function ($) {
+    "use strict";
+    $.fn.enableplaceholder = function () {
+
+        if ("placeholder" in document.createElement("input")){
+            return this;
+        }
+
+        return this.each(function () {
+            var $this = $(this);
+            
+            if ($this.val() === ""){
+                $this.val($this.attr("placeholder"));
+            }
+
+            $this
+            .focus(function () {
+                if ($this.val() === $this.attr("placeholder")){
+                    $this.val("");
+                }
+            })
+            .blur(function () {
+                if ($this.val() === ""){
+                    $this.val($this.attr("placeholder"));
+                }
+            })
+            .closest("form")
+            .bind("reset", function () {
+                $this.val($this.attr("placeholder"));
+                return false;
+            })
+            .bind("submit", function () {
+                if ($this.val() === $this.attr("placeholder")){
+                    $this.val("");
+                    setTimeout(function(){
+                        $this.val($this.attr("placeholder"));
+                    }, 0);
+                }
+            });
+        });
+    };
+})(jQuery);
